fe8a4058ffcb16247069b076144672703e9f026b
Calangiu <calangiualexandra22@stud.ase.ro>
2024-11-20 23:24:22 +0200

    Constructor cu parametri

--- Tema1/Tema1/Tema1.cpp
+++ Tema1/Tema1/Tema1.cpp
@@ -21,6 +21,38 @@ public:
         producator = new char[strlen("Mercedes") + 1];
         strcpy(producator, "Mercedes");
     }
+
+    Autobuz(int _capacitate, int _nrPersoaneImbarcate, char* _producator) : idAutobuz(nrAutobuze)
+    {
+        nrAutobuze++;
+        if (_capacitate <= 0) {
+            this->capacitate = 1;
+        }
+        else {
+            this->capacitate = _capacitate;
+        }
+
+        if (_nrPersoaneImbarcate > _capacitate) {
+            this->nrPersoaneImbracate = _capacitate;
+        }
+        else {
+            this->nrPersoaneImbracate = _nrPersoaneImbarcate;
+        }
+
+        if (strlen(_producator) == 0) {
+            producator = new char[strlen("Neidentificat") + 1];
+            strcpy(producator, "Neidentificat");
+        }
+        else {
+            producator = new char[strlen(_producator) + 1];
+            strcpy(producator, _producator);
+        }
+    }
+
+    ~Autobuz()
+    {
+        delete[] producator;
+    }
 };
 
 int Autobuz::nrAutobuze = 0;
@@ -28,4 +60,5 @@ int Autobuz::nrAutobuze = 0;
 int main()
 {
     Autobuz autobuzFaraParametri = Autobuz();
+    Autobuz autobuzCuParametri = Autobuz(15, 1, (char*)"Ford");
 }
